46d943ee0d39e263ec56cbc404ab9215ed4ba7f3
Ayana Powell <ayanapowell1@gmail.com>
2019-07-11 12:31:19 -0400

    Initial setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketList";
-import NewTicketForm from "./NewTicketForm";
-import { Switch, Route } from "react-router-dom";
 import Error404 from "./Error404";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -11,7 +9,6 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={TicketList} />
-        <Route path="/newticket" component={NewTicketForm} />
         <Route component={Error404} />
       </Switch>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="jumbotron">
-      <h1>Help queue!</h1>
-      <Link to="/">Home </Link> | <Link to="/newticket">Create Ticket</Link>
+      <h1>Agency</h1>
     </div>
   );
 }
